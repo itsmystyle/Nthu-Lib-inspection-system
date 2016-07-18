13721b8913b4b7baa2805cc088417208d741834e
Chong Ywuan Chai <itsmystyle.0821@gmail.com>
2016-07-18 16:51:55 +0800

    Splash Done

--- app/src/main/java/com/nthu/softwarestudio/app/nthulibraryinspectionsystem/Splash_Login_Activity.java
+++ app/src/main/java/com/nthu/softwarestudio/app/nthulibraryinspectionsystem/Splash_Login_Activity.java
@@ -2,12 +2,22 @@ package com.nthu.softwarestudio.app.nthulibraryinspectionsystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Splash_Login_Activity extends AppCompatActivity {
+
+    private EditText userName;
+    private EditText userPass;
+    private Button signIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_login);
+
+        userName = (EditText) findViewById(R.id.username_input);
+        userPass = (EditText) findViewById(R.id.password_input);
+        signIn = (Button) findViewById(R.id.Splash_SignIn);
     }
 }
