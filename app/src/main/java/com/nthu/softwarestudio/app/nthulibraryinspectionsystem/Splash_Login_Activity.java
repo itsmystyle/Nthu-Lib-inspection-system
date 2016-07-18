38c6a75611e6bbcfd92a545c2ff9af2952f4e7e8
Chong Ywuan Chai <itsmystyle.0821@gmail.com>
2016-07-18 08:32:19 +0800

    init

---
+++ app/src/main/java/com/nthu/softwarestudio/app/nthulibraryinspectionsystem/Splash_Login_Activity.java
@@ -0,0 +1,13 @@
+package com.nthu.softwarestudio.app.nthulibraryinspectionsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash_Login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_login);
+    }
+}
