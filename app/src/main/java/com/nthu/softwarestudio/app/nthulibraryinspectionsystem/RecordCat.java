91110f81584b642ece67d940d634702d65ef4f4f
dodaking <w666666we@gmail.com>
2016-07-19 17:09:37 +0800

    hahah

---
+++ app/src/main/java/com/nthu/softwarestudio/app/nthulibraryinspectionsystem/RecordCat.java
@@ -0,0 +1,13 @@
+package com.nthu.softwarestudio.app.nthulibraryinspectionsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecordCat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_cat);
+    }
+}
