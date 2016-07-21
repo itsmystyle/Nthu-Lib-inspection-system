7d842486c1a0c60bf16da3a20771d8ef08093c30
Chong Ywuan Chai <itsmystyle.0821@gmail.com>
2016-07-21 15:49:57 +0800

    Added dynamically generate button

--- app/src/main/java/com/nthu/softwarestudio/app/nthulibraryinspectionsystem/Device_Activity.java
+++ app/src/main/java/com/nthu/softwarestudio/app/nthulibraryinspectionsystem/Device_Activity.java
@@ -1,13 +1,19 @@
 package com.nthu.softwarestudio.app.nthulibraryinspectionsystem;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TableLayout;
+import android.widget.TableRow;
 
 import com.nthu.softwarestudio.app.nthulibraryinspectionsystem.Data.ViewContract;
 
 public class Device_Activity extends AppCompatActivity {
+    private final String LOG_TAG = getClass().getSimpleName();
 
     TableLayout tableLayout;
 
@@ -21,19 +27,83 @@ public class Device_Activity extends AppCompatActivity {
         int mode = getIntent().getExtras().getInt(ViewContract.MODE);
         int floor = getIntent().getExtras().getInt(ViewContract.FLOOR);
 
+        populateTableButton(mode, floor);
+    }
+
+    private void populateTableButton(int mode, int floor) {
+        int tableRow = 4;
+        int tableCol = 3;
+
         if(mode == ViewContract.INSPECTION){
-            populateInspectionTableButton(floor);
+            for(int row = 0; row < tableRow; row++){
+                TableRow tRow = new TableRow(this);
+                tRow.setLayoutParams(new TableLayout.LayoutParams(
+                        TableLayout.LayoutParams.MATCH_PARENT,
+                        TableLayout.LayoutParams.MATCH_PARENT,
+                        0.1f
+                ));
+                tableLayout.addView(tRow);
+
+                for(int col = 0; col < tableCol; col++){
+                    Button tableButton = new Button(this);
+                    tableButton.setLayoutParams(new TableRow.LayoutParams(
+                            TableRow.LayoutParams.MATCH_PARENT,
+                            TableRow.LayoutParams.MATCH_PARENT,
+                            0.1f
+                    ));
+                    tableButton.setText(row + "," + col);
+                    tableButton.setTextSize(getResources().getDimension(R.dimen.tableButtonSize));
+                    tableButton.setBackgroundResource(R.drawable.rounded_button_menu);
+
+                    tRow.addView(tableButton);
+                }
+            }
+
+            TableRow tRow = new TableRow(this);
+            tRow.setLayoutParams(new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    0.1f
+            ));
+            tableLayout.addView(tRow);
+            for(int col = 0; col < tableCol; col++){
+                Button tableButton = new Button(this);
+                tableButton.setLayoutParams(new TableRow.LayoutParams(
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        0.1f
+                ));
+                tableButton.setText(4 + "," + col);
+                tableButton.setTextSize(getResources().getDimension(R.dimen.tableButtonSize));
+                tableButton.setBackgroundResource(R.drawable.rounded_button_menu);
+                if(col == 2) tableButton.setVisibility(View.INVISIBLE);
+
+                tRow.addView(tableButton);
+            }
+
         }else{
-            populateHistoryTableButton(floor);
+            for(int row = 0; row < tableRow; row++){
+                TableRow tRow = new TableRow(this);
+                tRow.setLayoutParams(new TableLayout.LayoutParams(
+                        TableLayout.LayoutParams.MATCH_PARENT,
+                        TableLayout.LayoutParams.MATCH_PARENT,
+                        0.1f
+                ));
+                tableLayout.addView(tRow);
+
+                for(int col = 0; col < tableCol; col++){
+                    Button tableButton = new Button(this);
+                    tableButton.setLayoutParams(new TableLayout.LayoutParams(
+                            TableRow.LayoutParams.MATCH_PARENT,
+                            TableRow.LayoutParams.MATCH_PARENT,
+                            0.1f
+                    ));
+                    tableButton.setText(tableRow + "," + tableCol);
+                    tRow.addView(tableButton);
+                }
+            }
+
         }
-
     }
 
-    private void populateHistoryTableButton(int floor) {
-
-    }
-
-    private void populateInspectionTableButton(int floor) {
-
-    }
 }
